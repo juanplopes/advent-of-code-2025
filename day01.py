d22bea60750b499dcc2b5ed7145b6b2c5e8574f6
Juan Lopes <juan.lopes@zwift.com>
2025-12-01 14:08:05 -0300

    Improved D1

--- day01.py
+++ day01.py
@@ -1,12 +1,12 @@
 import sys
-def solve(line):
-    return (-1 if line[0] == 'L' else 1) * int(line[1:])
     
 state, answer1, answer2 = 50, 0, 0
 for line in sys.stdin.read().splitlines():
-    clicks = solve(line)
+    clicks = (-1 if line[0] == 'L' else 1) * int(line[1:])
+    
     if clicks < 0: answer2 += abs((state + clicks-1) // 100) - (state == 0)
     elif clicks > 0: answer2 += abs((state + clicks) // 100)
+    
     state = (state + clicks) % 100
     if state == 0: answer1 += 1
    
