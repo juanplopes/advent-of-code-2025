7be826a6ed1297f83e294ec2b5e5535e868b2938
Juan Lopes <juan.lopes@zwift.com>
2025-12-04 19:58:53 -0300

    Day 4

--- day04.py
+++ day04.py
@@ -1,19 +1,15 @@
 import sys, functools
 
 def remove(T):
-    answer = 0
-    M = {(i, j): T[i][j] == '@' for i in range(len(T)) for j in range(len(T[i]))}
-    for (i, j) in M.keys():
-        if T[i][j] != '@': continue
-        if sum(M.get((i+a, j+b), 0) for a in range(-1, 2) for b in range(-1, 2) if a or b) < 4:
-            answer += 1
+    S = {(i, j) for i in range(len(T)) for j in range(len(T[i])) if T[i][j] == '@'}
+    for (i, j) in S:
+        if sum((i+a, j+b) in S for a in (-1,0,1) for b in (-1,0,1)) < 5:
+            yield 1
             T[i][j] = '.'
-    return answer
-
 
 T = [list(s) for s in sys.stdin.read().splitlines()]
-answers = [remove(T)]
+answers = [sum(remove(T))]
 while answers[-1] != 0:
-    answers += [remove(T)]
+    answers += [sum(remove(T))]
 
 print(answers[0], sum(answers))
