56f6c9581524d41b9db8a1345641247e445f792c
Juan Lopes <juan.lopes@zwift.com>
2025-12-08 15:27:47 -0300

    Day 088

--- day08.py
+++ day08.py
@@ -1,10 +1,8 @@
 import sys, collections, math, heapq
 
 V = [list(map(int, l.split(','))) for l in sys.stdin.read().splitlines()]
-E = []
-for i in range(len(V)):
-    for j in range(i+1, len(V)):
-        E.append((sum((V[i][k]-V[j][k])**2 for k in range(3)) ** 0.5, i, j))
+E = [(sum((V[i][k]-V[j][k])**2 for k in range(3)) ** 0.5, i, j)
+     for i in range(len(V)) for j in range(i+1, len(V))]
 heapq.heapify(E)
 
 P = list(range(len(V)))
@@ -17,14 +15,15 @@ count1, count2, answer1, answer2 = 0, 0, 0, 0
 while count2 < len(V)-1:
     _, i, j = heapq.heappop(E)
 
-    answer2 = V[i][0] * V[j][0]
     count1 += 1
     if count1 == 1000: 
         answer1 = math.prod(heapq.nlargest(3, 
             collections.Counter(map(find, range(len(V)))).values()))
-    
+
+
     a, b = find(i), find(j)
     P[min(a, b)] = max(a, b)
     if a != b: count2 += 1
+    answer2 = V[i][0] * V[j][0]
 
 print(answer1, answer2)
